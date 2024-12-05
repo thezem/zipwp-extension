63b30038915f96a94dbf5928c302fb08e4890eef
thezem <looza.zooma@gmail.com>
2024-12-05 12:51:14 +0200

    Remove background script and update manifest permissions for improved extension performance

--- ZipWp/popup.js
+++ ZipWp/popup.js
@@ -1,12 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const createBlankSiteBtn = document.getElementById('create-new-blank-site');
-  const createAiSiteBtn = document.getElementById('create-new-ai-site');
+const createBlankSiteBtn = document.getElementById('create-new-blank-site');
+const createAiSiteBtn = document.getElementById('create-new-ai-site');
 
-  createAiSiteBtn.addEventListener('click', function () {
-    chrome.tabs.create({ url: 'https://app.zipwp.com/create-complete-site/type?source=zipwp&medium=zipwp-browser-extension' });
-  });
+createAiSiteBtn.addEventListener('click', function () {
+  chrome.tabs.create({ url: 'https://app.zipwp.com/create-complete-site/type?source=zipwp&medium=zipwp-browser-extension' });
+});
 
-  createBlankSiteBtn.addEventListener('click', function () {
-    chrome.tabs.create({ url: 'https://try.new/' });
-  });
+createBlankSiteBtn.addEventListener('click', function () {
+  chrome.tabs.create({ url: 'https://try.new/' });
 });
